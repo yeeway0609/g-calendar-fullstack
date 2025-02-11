b581a565c693d4ca5dd035d840fdd0e90746a33f
yeeway <yiwei.suuu@gmail.com>
2025-02-11 14:23:48 +0800

    feat: add Vite React plugin and remove Google OAuth provider from main entry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const CLIENT_ID = "521320255264-3q0f5dcnh9ebif8d21lia87ctjf8kkij.apps.googleusercontent.com"
-
-createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={CLIENT_ID}>
-    <App />
-  </GoogleOAuthProvider>
-);
+createRoot(document.getElementById("root")).render(<App />);
